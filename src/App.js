5a2aec5fee42df796459a90fef146e502ebbaa2e
BennyDagur <bdg2@nemandi.ntv.is>
2022-11-09 22:31:56 +0000

    Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Head = (props) => {
+  return <h2 className="TextProps">{props.text}</h2>
+}
+
+const App = () => {
+  const name = "Benny"
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="MainDiv">
+        <div>
+        <h1 className="TextProps">My name Is {name}</h1>
+        <Head text="Lorem ipsum dolor sit amet" />
+        </div>
+        
+        <img className="ImgProps" src="Yarrick.jpeg" alt="The Great Yarrick"></img>
+        </div>
+        <hr></hr>
+        <div>
+        <h1 className="TextProps">Oddi</h1>
+        <h2 className="TextProps">2020 - Nú</h2>
+        <p className="TextProps">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nullam rhoncus varius mi ut blandit. Fusce ultrices tellus eu vulputate 
+          ullamcorper. Donec pulvinar hendrerit neque vitae fermentum. Nulla semper 
+          at purus vitae auctor. Fusce id consequat purus. Phasellus dignissim 
+          ultricies odio. Suspendisse lacinia vel massa eget laoreet. Aliquam commodo 
+          vel leo quis eleifend. Cras ut eleifend mauris. Donec pulvinar eu sem quis 
+          interdum.</p>
+        </div>
+        <div>
+        <h1 className="TextProps">Oddi</h1>
+        <h2 className="TextProps">2020 - Nú</h2>
+        <p className="TextProps">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nullam rhoncus varius mi ut blandit. Fusce ultrices tellus eu vulputate 
+          ullamcorper. Donec pulvinar hendrerit neque vitae fermentum. Nulla semper 
+          at purus vitae auctor. Fusce id consequat purus. Phasellus dignissim 
+          ultricies odio. Suspendisse lacinia vel massa eget laoreet. Aliquam commodo 
+          vel leo quis eleifend. Cras ut eleifend mauris. Donec pulvinar eu sem quis 
+          interdum.</p>
+        </div>
+        <div>
+        <h1 className="TextProps">Oddi</h1>
+        <h2 className="TextProps">2020 - Nú</h2>
+        <p className="TextProps">Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+          Nullam rhoncus varius mi ut blandit. Fusce ultrices tellus eu vulputate 
+          ullamcorper. Donec pulvinar hendrerit neque vitae fermentum. Nulla semper 
+          at purus vitae auctor. Fusce id consequat purus. Phasellus dignissim 
+          ultricies odio. Suspendisse lacinia vel massa eget laoreet. Aliquam commodo 
+          vel leo quis eleifend. Cras ut eleifend mauris. Donec pulvinar eu sem quis 
+          interdum.</p>
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
